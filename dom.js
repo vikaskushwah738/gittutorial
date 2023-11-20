7f3db829baef772536e32fbf738c3d34e1c16963
vikaskushwah738 <vikaskushwah738@gmail.com>
2023-11-20 23:40:19 +0530

    second

--- dom.js
+++ dom.js
@@ -18,14 +18,19 @@
 //get document by id
 
 //console.log(document.getElementById('header-title'));
-let headerTitle= document.getElementById('header-title');
-let header= document.getElementById('main-header');
-console.log(headerTitle);
-
-header.style.borderBottom ="solid 3px black"
+// let headerTitle= document.getElementById('header-title');
+// let header= document.getElementById('main-header');
+// console.log(headerTitle);
+// header.style.borderBottom ="solid 3px black"
 //headerTitle.innerHTML="<h3>Hello</h3>";
-let item=document.getElementById('items');
-item.style.fontWeight='bold';
-item.style.color='green';
+// let item=document.getElementById('items');
+// item.style.fontWeight='bold';
+// item.style.color='green';
 
+// get element by classname
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight='bold';
+}
 
